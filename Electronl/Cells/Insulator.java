640207e81cf6b18f323453386e99553862f10a81
Scratchy <32627001+Scratchdragon@users.noreply.github.com>
2021-12-17 18:53:13 +1300

    Add files via upload

---
+++ Electronl/Cells/Insulator.java
@@ -0,0 +1,21 @@
+package Electronl.Cells;
+
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.image.*;
+
+import java.io.*;
+import javax.imageio.*;
+
+import javax.swing.*;
+import Electronl.*;
+
+public class Insulator extends Module {
+    public Insulator() {
+        name = "Insulator";
+    }
+    @Override
+    public Image getSprite() {
+        return (Frame.toolkit.getImage(Frame.imagesdir + "Insulator.png"));
+    }
+}
